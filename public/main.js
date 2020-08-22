aa8a0da6ad1acdea6ff2e55e2ea855aa879d5c68
祖納元りえ <br061422@gmail.com>
2020-08-22 22:15:44 +0900

    sidemenu_responsive

--- public/main.js
+++ public/main.js
@@ -1,8 +1,11 @@
-// $('.btn').on('click', function(){
+$('.btn').on('click', function(){
 
-// 	$('.btn__menu').toggleClass('active');
-// 	$('.p-global-nav').toggleClass('active');
-// 	$('.menu-contents').toggleClass('active');
+	$('.btn__menu').toggleClass('active');
+	$('.p-global-nav').toggleClass('active');
+	$('.menu-contents').toggleClass('active');
+    $('.menu-content').toggleClass('active');
+    $('.worker-info').toggleClass('active');
+
 
     
-// });
+});
