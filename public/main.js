092f294dc22f160d77c157ed07b4008474f7380f
祖納元りえ <br061422@gmail.com>
2020-08-23 22:31:04 +0900

    sidemenu_responsive_背景を黒に

--- public/main.js
+++ public/main.js
@@ -1,11 +1,10 @@
 $('.btn').on('click', function(){
 
 	$('.btn__menu').toggleClass('active');
-	$('.p-global-nav').toggleClass('active');
+    $('.p-global-nav').toggleClass('active');
+    $('.side-menu').toggleClass('active');
 	$('.menu-contents').toggleClass('active');
     $('.menu-content').toggleClass('active');
-    $('.worker-info').toggleClass('active');
-
 
     
 });
